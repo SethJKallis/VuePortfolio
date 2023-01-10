2daba48dfea3861563d28e91dac975fe7d41b793
SethJKallis <kallisseth@gmail.com>
2023-01-10 10:18:52 +0200

    used directives too loop through and display testimonials and projects

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -8,8 +8,8 @@
         data-bs-ride="carousel"
       >
         <div class="carousel-inner">
-          <div
-            class="carousel-item active itemOne position-relative"
+          <div v-for="testimonial in testimonials" v-bind:key="testimonial"
+            class="carousel-item" :class="testimonial.class"
             data-bs-interval="5000"
           >
             <div
@@ -19,102 +19,12 @@
               <div class="flip-card">
                 <figure class="flip-card-inner">
                   <div class="flip-card-front">
-                    <img src="https://i.postimg.cc/52Mzr9Hf/Camryn.jpg" alt="Camryn" />
+                    <img v-bind:src="testimonial.imageUrl" :alt="testimonial.imageAlt" />
                   </div>
                   <div class="flip-card-back">
-                    <p>
-                      "Seth Kallis is an exceptional person and colleague, very
-                      down to earth. He grasps new ideas very quickly which
-                      makes him a very valuable asset to any organization that
-                      is lucky to have him." <br /><br />-Camryn Herandien
-                    </p>
+                    <p>{{ testimonial.comment }}</p>
                   </div>
                 </figure>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item itemTwo" data-bs-interval="5000">
-            <div class="container d-flex justify-content-center">
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <figure class="flip-card-front">
-                    <img src="https://i.postimg.cc/ZR0vNCQx/Matthew.jpg" alt="Mathew Thomas" />
-                  </figure>
-                  <div class="flip-card-back">
-                    <p>
-                      "Seth is quite a creative and open minded colleague that
-                      works well with the task at hand. He thinks of others
-                      without compromising his ethical integrity." <br /><br />
-                      -Matthew Thomas
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item itemThree" data-bs-interval="5000">
-            <div class="container d-flex justify-content-center">
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <figure class="flip-card-front">
-                    <img src="https://i.postimg.cc/76cGnq88/Lazola.jpg" alt="Lazola" />
-                  </figure>
-                  <div class="flip-card-back">
-                    <p>
-                      "Seth is a fast learner. He is always eager and inclined
-                      to know more. He is a team player and can be a valuable
-                      asset to your company." <br /><br />-Lazola Makubalo
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item itemFour" data-bs-interval="5000">
-            <div class="container d-flex justify-content-center">
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <figure class="flip-card-front">
-                    <img src="https://i.postimg.cc/TPtWhrtP/faeezy.jpg" alt="Faeezy" />
-                  </figure>
-                  <div class="flip-card-back">
-                    <p>
-                      "Seth is a hardworking person, is committed to the task at
-                      hand and never let's anything get in his way. A
-                      hard-working person focused on growth, knowledge and is
-                      diligent worker." <br /><br />-Faeez Miller
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item itemFive" data-bs-interval="5000">
-            <div class="container d-flex justify-content-center">
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <figure class="flip-card-front">
-                    <img
-                      class="d-inline"
-                      src="https://i.postimg.cc/xjwt3z2C/Joel-Profile.jpg"
-                      alt="Joel"
-                    />
-                  </figure>
-                  <div class="flip-card-back">
-                    <p>
-                      "Seth is a dedicated, creative, go-getter, and
-                      well-behaved student. He enjoys working with his
-                      classmates and works well on his project. He has gained
-                      knowledge of web development, including Bootstrap 5. I
-                      will be very delighted to see him get hired as a
-                      developer." <br /><br />-Joel Mukanya
-                    </p>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
@@ -143,7 +53,19 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: "testimonialsComponent",
+  computed: {
+    ...mapGetters(["testimonials"])
+  },
+  methods: {
+    ...mapActions(["fetchTestimonials"]),
+
+  },
+  created(){
+    this.fetchTestimonials();
+  }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,46 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    projects: null,
+    testimonials: null
   },
   getters: {
+    testimonials(state){
+      return state.testimonials
+    },
+    projects(state){
+      return state.projects
+    }
   },
   mutations: {
+    setTestimonials(state, testimonials){
+      state.testimonials = testimonials
+    },
+    setProjects(state, projects){
+      state.projects = projects
+    }
   },
   actions: {
+    fetchTestimonials(content){
+      return fetch('https://sethjkallis.github.io/Vue-Portfolio-Data/testimonials.json')
+      .then((response) => response.json())
+      .then((data) => {
+        content.commit("setTestimonials", data.testimonials)
+      })
+      .catch((err) => {
+        console.warn(err)
+      })
+    },
+    fetchProjects(context){
+      return fetch('https://sethjkallis.github.io/Vue-Portfolio-Data/projects.json')
+      .then((response) => response.json())
+      .then((data) => {
+        context.commit("setProjects", data.projects)
+      })
+      .catch((err) => {
+        console.warn(err)
+      })
+    }
   },
   modules: {
   }
